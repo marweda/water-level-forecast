52b9a7ca550572632efd51f380965108d6a9e54d
Marcel Gaweda <github@mgaweda.de>
2025-07-09 00:11:22 +0200

    DWD REST Endpoints

---
+++ src/api/dwd.py
@@ -0,0 +1,36 @@
+from .base import BaseEndpoint
+
+
+class DWDEndpoint(BaseEndpoint):
+    BASE_URL = "https://opendata.dwd.de/"
+
+    def __init__(self, resource_path: str):
+        self.resource_path = resource_path
+
+    @property
+    def url(self) -> str:
+        return f"{self.BASE_URL}{self.resource_path}"
+
+
+class DWDPrecipitationHourlyStations(DWDEndpoint):
+    def __init__(self):
+        resource = "climate_environment/CDC/observations_germany/climate/hourly/precipitation/recent/RR_Stundenwerte_Beschreibung_Stationen.txt"
+        super().__init__(resource)
+
+
+class DWDPrecipitationHourlyData(DWDEndpoint):
+    def __init__(self, station_id: int):
+        resource = f"climate_environment/CDC/observations_germany/climate/hourly/precipitation/recent/stundenwerte_RR_{station_id}_akt.zip"
+        super().__init__(resource)
+
+
+class DWDRadolanRW(DWDEndpoint):
+    def __init__(self):
+        resource = "weather/radar/radolan/rw/raa01-rw_10000-latest-dwd---bin.bz2"
+        super().__init__(resource)
+
+
+class DWDRadvorRQ(DWDEndpoint):
+    def __init__(self, creation_time: int, forecast_span: int):
+        resource = f"weather/radar/radvor/rq/R{creation_time}_{forecast_span}.gz"
+        super().__init__(resource)
